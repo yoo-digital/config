4b1bc0f80cdcdde621f1e76232acb01fb76509d8
Tobias Maier (mat) <tobias.maier@yoo.digital>
2025-09-11 13:26:53 +0200

    feat(example-vite-react): simplify react-vite-app eslint-config

--- examples/react-app/eslint.config.js
+++ examples/react-app/eslint.config.js
@@ -1,16 +1,4 @@
 import yooEslintConfigReact from '@yoo-digital/eslint-config-react';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
 
-export default tseslint.config([
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [...yooEslintConfigReact, reactRefresh.configs.vite],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
-]);
+export default [...yooEslintConfigReact, reactRefresh.configs.vite];
